27e34afb3ab93eb6752845c83df49900786f0a38
ganbin <jlau.ice@gmail.com>
2025-09-21 16:45:29 +0800

    add: 数据库表设计

--- server/sql/create_table.sql
+++ server/sql/create_table.sql
@@ -28,52 +28,46 @@ create table if not exists user
     index idx_union_id (union_id)
 ) comment '用户' collate = utf8mb4_unicode_ci;
 
--- 题目表
-create table if not exists question
+-- 音频文件表
+create table if not exists audio_files
 (
-    id           bigint auto_increment comment 'id' primary key,
-    title        varchar(512)                       null comment '标题',
-    content      text                               null comment '内容',
-    difficulty   varchar(255)                       null comment '难度',
-    tags         json                               null comment '标签列表（json 数组）',
-    answer       text                               null comment '题目答案',
-    submit_num   int      default 0                 not null comment '题目提交数',
-    accepted_num int      default 0                 not null comment '题目通过数',
-    judge_case   json                               null comment '判题用例（json 数组）',
-    judge_config json                               null comment '判题配置（json 对象）',
-    thumb_num    int      default 0                 not null comment '点赞数',
-    favour_num   int      default 0                 not null comment '收藏数',
-    user_id      bigint                             not null comment '创建用户 id',
-    temp1        varchar(255)                       null comment '保留字段1',
-    temp2        varchar(255)                       null comment '保留字段2',
-    temp3        varchar(255)                       null comment '保留字段3',
-    create_time  datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    update_time  datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    is_delete    tinyint  default 0                 not null comment '是否删除',
-    index idx_userId (user_id)
-) comment '题目' collate = utf8mb4_unicode_ci;
+    id          bigint auto_increment comment 'id' primary key,
+    user_id     bigint                             not null comment '创建用户 id',
+    file_name   varchar(256)                       not null comment '文件名',
+    file_path   varchar(1024)                      not null comment '文件路径',
+    file_size   int                                not null comment '文件大小(kb)',
+    duration    int      default 0                 not null comment '音频时长',
+    file_type   varchar(256)                       not null comment '文件类型(wav,mp3..)',
+    status      int      default 0                 not null comment '文件状态（0 - 待处理、1 - 处理中、2 - 处理成功、3 - 处理失败）',
+    temp1       varchar(255)                       null comment '保留字段1',
+    temp2       varchar(255)                       null comment '保留字段2',
+    temp3       varchar(255)                       null comment '保留字段3',
+    create_time datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    update_time datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    is_delete   tinyint  default 0                 not null comment '是否删除'
+) comment '音频文件' collate = utf8mb4_unicode_ci;
 
 
--- 题目提交表
-create table if not exists question_submit
+-- 转写任务表
+create table if not exists audio_files
 (
-    id               bigint auto_increment comment 'id' primary key,
-    question_id      bigint                             not null comment '题目 id',
-    user_id          bigint                             not null comment '创建用户 id',
-    language         varchar(128)                       not null comment '编程语言',
-    code             text                               not null comment '用户代码',
-    judge_info       json                               null comment '判题信息（json 对象）执行时间等等',
-    judgment_results text                               null comment '判题结果枚举值 ',
-    status           int      default 0                 not null comment '判题状态（0 - 待判题、1 - 判题中、2 - 成功、3 - 失败）',
-    temp1            varchar(255)                       null comment '保留字段1',
-    temp2            varchar(255)                       null comment '保留字段2',
-    temp3            varchar(255)                       null comment '保留字段3',
-    create_time      datetime default CURRENT_TIMESTAMP not null comment '创建时间',
-    update_time      datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
-    is_delete        tinyint  default 0                 not null comment '是否删除',
-    index idx_questionId (question_id),
-    index idx_userId (user_id)
-) comment '题目提交';
+    id                bigint auto_increment comment 'id' primary key,
+    user_id           bigint                             not null comment '创建用户 id',
+    file_id           bigint                             not null comment '音频文件 id',
+    model_name        varchar(256)                       null comment '模型名称',
+    interface_address varchar(1024)                      null comment '接口地址',
+    execution_time    bigint                             null comment '执行时间',
+    status            int      default 0                 not null comment '任务状态（0 - 待处理、1 - 处理中、2 - 处理成功、3 - 处理失败）',
+    result            text                               null comment '结果',
+    temp1             varchar(255)                       null comment '保留字段1',
+    temp2             varchar(255)                       null comment '保留字段2',
+    temp3             varchar(255)                       null comment '保留字段3',
+    create_time       datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    update_time       datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    is_delete         tinyint  default 0                 not null comment '是否删除'
+) comment '音频文件' collate = utf8mb4_unicode_ci;
+
+
 -- 帖子表
 create table if not exists post
 (
