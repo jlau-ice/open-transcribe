698a6e6e34b3952510c7351477adbe36369684c2
ganbin <jlau.ice@gmail.com>
2025-09-21 19:35:50 +0800

    fix: 样式微调

--- web-ui/src/views/home/UploadContent.vue
+++ web-ui/src/views/home/UploadContent.vue
@@ -3,8 +3,8 @@
     <div class="flex flex-col items-center">
       <h1 class="text-[#262626] text-[48px] font-bold leading-tight m-0">免费音频转文本</h1>
       <span class="text-[#8c8c8c] text-[18px] leading-tigh mt-[8px]">使用我们的免费音频转文本工具，即刻将音频转为文本。完美支持语音转文本、声音转文本以及AI驱动的转录工具。</span>
-      <div class="mt-[60px] mx-[auto]">
-        <div class="flex flex-col items-center border-[1px] border-[#bdbdbd] rounded-[20px] p-[32px] bg-[#fafafa]">
+      <div class="mt-[60px] mx-[auto] min-w-[840px]">
+        <div class="flex flex-col items-center border-[1px] border-[#bdbdbd] border-dashed rounded-[20px] p-[32px] bg-[#fafafa]">
           <img class="max-h-[108px]" src="@/assets/file/upload.png" />
           <div class="p-[10px] flex justify-center items-center gap-[10px] mt-[12px] cursor-pointer bg-[#222226] hover:bg-[#000] rounded-[10px] min-w-[328px] h-[48px]">
             <svg fill="none" style="min-width: 20px; min-height: 20px" width="20" height="20" viewBox="0 0 20 20">
--- web-ui/src/views/task/TaskManage.vue
+++ web-ui/src/views/task/TaskManage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>task</div>
+  <div class="p-5">task</div>
 </template>
 <script setup lang="ts"></script>
 <style scoped lang="scss"></style>
